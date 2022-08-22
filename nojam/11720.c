a5f1faa4eafd87908101bd0ba9ba917a504e4ea4
cod0216 <cod0216@nate.com>
2022-08-22 09:27:51 +0900

    nojam

---
+++ nojam/11720.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+int n, sum=0; 
+
+
+scanf("%d", &n);
+
+char a[n];
+scanf("%s", a);
+
+for(int i = 0; i < n; i++)
+	sum = a[i]+sum -'0';
+
+
+printf("%d", sum);
+
+return 0;
+}
