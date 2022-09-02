a23bf8525e8eb2c2a3df16f051e544c753dbb6df
cod0216 <cod0216@nate.com>
2022-09-02 19:39:48 +0900

    TIL commit

---
+++ 알쏭달쏭C언어/Array/7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define NUMBER 96
+
+int main()
+{
+	int i, num;
+	int a[NUMBER];
+
+
+	printf("데이터 수 : ");
+
+	do{
+		scanf("%d", &num);
+		if(num < 1 || num > NUMBER)
+			printf("\a1~%d 사이로 입력해주세요. ", NUMBER);
+		} while (num < 1 || num> NUMBER);
+
+		for(i = 0; i < num; i++) {
+			printf("%2d번 : ", i+1);
+			scanf("%d", &a[i]);
+		}
+
+		printf("{");
+
+		for(i = 0; i < num - 1; i++)
+			printf("%d, ", a[i]);
+
+		printf("%d}\n", a[i]);
+
+		return 0;
+	}
