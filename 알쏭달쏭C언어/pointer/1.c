96dbbc6632c2479ef0a895f5427f498a0a9a2e23
cod0216 <cod0216@nate.com>
2022-09-04 17:50:01 +0900

    TIL pointer commit

---
+++ 알쏭달쏭C언어/pointer/1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void adjust_point(int *n)
+{
+	if(*n < 0) * n = 0;
+	if(*n > 100) *n = 100;
+}
+
+int main()
+{
+	int point;
+
+	printf("시험 점수 : ");
+	scanf("%d", &point);
+
+	adjust_point(&point);
+
+	printf("점수는 %d점 입니다.\n", point);
+
+	return 0;
+}
+
+	
