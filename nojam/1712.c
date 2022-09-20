07279818f1b0d021bef6f3c1fad7e869bdbd5d02
cod0216 <cod0216@nate.com>
2022-09-20 17:51:59 +0900

    TIL pointer commit

--- nojam/1712.c
+++ nojam/1712.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+int sum(int a, int b){
+	int s = 0;
+	s = a + b;
+	return s;
+}
 
-
-unsigned long int Find_BE_Point(int x, int y)
-{
-	if(y > x)
+int ans(int a, int b){
+	
+	if (a < b)
 		return 1;
 	else return 0;
 }
@@ -12,30 +16,31 @@ unsigned long int Find_BE_Point(int x, int y)
 int main()
 {
 
-	unsigned long int sum = 0 ;
 	int a, b, c;
-	unsigned long int i = 0;
-	unsigned long int v = 0;
-	unsigned long int b1, c1;
-
-	scanf("%d", &a);
-	scanf("%d", &b);
-	scanf("%d", &c);
-		
-	 
-		for(i = 0 ; i <2100000000; i++) {
-			b1 = b * i;
-			c1 = c * i;
-			v = a+b1;
-			if (Find_BE_Point(v, c1) == 1){
-			printf("v : %ld a+b1 : %ld c1 : %ld a : %d b : %d b1: %ld c : %d \n", v, a+b1, c1, a, b, b1, c);
-				printf("%ld",i);
-				return 0; 
-			}
-			
-		}
+	int v = 0;
+	int m = 0;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d %d\n", a*(b*2100000001), c*2100000001);
+/*	if(a*(b*2100000001) >= c*2100000001){
 		printf("-1");
-	
+		return 0;
+	}
+
+	for(int i = 1 ; i < 2100000002; i++)
+	{	
+		printf("a : %d b : %d c : %d \n", a, b, c);
+		int	d = b * i;
+		int f = c * i;
+		printf("d : %d\n",d);
+		m = sum(a, d);
+		printf("m : %d c : %d\n", m, c);
+		if (ans(m,f) == 1){
+			printf("%d\n", i);
+			return 0;
+		}
+	}
+	printf("-1");*/
 	return 0;
 }
+
 
